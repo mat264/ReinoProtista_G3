5ef5572d4ec897c427075b6f962bae6d7ce89561
023daniii <agb.dan.zzz@gmail.com>
2025-11-11 22:14:30 -0600

    .2

--- ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Ameba.java
+++ ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Ameba.java
@@ -1,4 +1,70 @@
 package pkBioProto.pkReinoViviente.pkProtoctista;
-public class Ameba extends Rizopodos {
 
+public class Ameba extends Rizopodos {
+    private String tipoMovimiento;
+    private String habitad;
+    
+    public Ameba() {
+        super();
+        this.tipoMovimiento = "Pseudópodos";
+        this.habitad = "Agua dulce";
+    }
+    
+    public Ameba(String nombre, String tipoMovimientoProtozoos, String habitat, String tipoMovimiento, String habitad) {
+        super(nombre, tipoMovimientoProtozoos, habitat);
+        this.tipoMovimiento = tipoMovimiento;
+        this.habitad = habitad;
+    }
+    
+    public Ameba(String habitad) {
+        super("Ameba Proteus", "Pseudópodos", habitad);
+        this.tipoMovimiento = "Pseudópodos";
+        this.habitad = habitad;
+    }
+    
+    // Métodos específicos de Ameba
+    public void fagocitar() {
+        System.out.println("La ameba está fagocitando alimento mediante pseudópodos");
+    }
+    
+    @Override
+    public void reproducirse() {
+        System.out.println("La ameba se está reproduciendo por división binaria");
+    }
+    
+    // Implementación de métodos abstractos de Rizopodos
+    @Override
+    public void desplazarse() {
+        System.out.println("La ameba se está desplazando mediante " + this.tipoMovimiento);
+    }
+    
+    @Override
+    public void capturarAlimento() {
+        System.out.println("La ameba está capturando alimento extendiendo pseudópodos");
+        fagocitar();
+    }
+    
+    // Implementación de métodos abstractos de Protozoos
+    @Override
+    public void alimentarse() {
+        System.out.println("La ameba se está alimentando por fagocitosis en " + this.habitad);
+    }
+    
+    @Override
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+    
+    @Override
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+    
+    public String getHabitad() {
+        return habitad;
+    }
+    
+    public void setHabitad(String habitad) {
+        this.habitad = habitad;
+    }
 }
--- ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Rizopodos.java
+++ ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Rizopodos.java
@@ -1,5 +1,39 @@
 package pkBioProto.pkReinoViviente.pkProtoctista;
 
-public abstract class Rizopodos extends Protozoos{
-
+public abstract class Rizopodos extends Protozoos {
+    private String estructuraMovimiento;
+    private String tipoReproduccion;
+    
+    public Rizopodos() {
+        super();
+        this.estructuraMovimiento = "Pseudópodos";
+        this.tipoReproduccion = "División binaria";
+    }
+    
+    public Rizopodos(String nombre, String tipoMovimiento, String habitat) {
+        super(nombre, tipoMovimiento, habitat);
+        this.estructuraMovimiento = "Pseudópodos";
+        this.tipoReproduccion = "División binaria";
+    }
+    
+    // Métodos abstractos que debe implementar Ameba
+    @Override
+    public abstract void desplazarse();
+    public abstract void capturarAlimento();
+    
+    public String getEstructuraMovimiento() {
+        return estructuraMovimiento;
+    }
+    
+    public void setEstructuraMovimiento(String estructuraMovimiento) {
+        this.estructuraMovimiento = estructuraMovimiento;
+    }
+    
+    public String getTipoReproduccion() {
+        return tipoReproduccion;
+    }
+    
+    public void setTipoReproduccion(String tipoReproduccion) {
+        this.tipoReproduccion = tipoReproduccion;
+    }
 }
