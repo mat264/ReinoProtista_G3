af715bfe163fcc4f19596feb65298b1600aff996
451750 <mathias.mejia@epn.edu.ec>
2025-11-12 01:36:50 -0500

    Cambio

--- ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Euglena.java
+++ ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Euglena.java
@@ -1,24 +1,17 @@
 package pkBioProto.pkReinoViviente.pkProtoctista;
 
-// Esta es tu clase para el R01.
-// Hereda de Flagelados.
 public class Euglena extends Flagelados {
 
-    // Atributo propio para el R01 (en espanol)
     private boolean hayLuz;
 
-    // Constructor de Euglena
-    // (Este es el constructor que tu AppBioProto.java está buscando)
+
     public Euglena(String nombre, String habitat, boolean hayLuz) {
-        // Llamo al constructor padre (Flagelados)
+
         super(nombre, habitat, 1);
         this.hayLuz = hayLuz;
     }
 
-    // --- Metodos obligatorios y sobrescritos ---
 
-    // R01: Implementacion de alimentarse
-    // Corregido a 'void' para ser compatible
     @Override
     public void alimentarse() {
         String modoAlimentacion;
@@ -29,44 +22,41 @@ public class Euglena extends Flagelados {
             modoAlimentacion = "absorbe materia orgánica (heterótrofa).";
         }
         
-        // Usamos getNombre() y System.out.println()
+
         System.out.println(getNombre() + " " + modoAlimentacion);
     }
 
-    // Sobrescribimos la 'accion' de SerVivo
+
     @Override
     public void reaccionarEstimulo(String estimulo) {
-        // Comprobamos si el estímulo es la luz
+
         if (estimulo.equalsIgnoreCase("luz")) {
-            // Si es luz, llamamos a nuestro método único
+            // Si es luz, llamamos a nuestro método 
             this.orientarHaciaLaLuz();
         } else {
-            // Si es otro estímulo, usamos la respuesta genérica
+            // Si es otro estímulo 
             System.out.println(getNombre() + " reacciona a " + estimulo + " pero no se mueve.");
         }
     }
 
 
-    // --- Metodos propios de Euglena (R01) ---
+    // --- Metodos propios
     
-    // Corregido a 'void'
     public void orientarHaciaLaLuz() {
         if (hayLuz) {
-            // Usamos getNombre() y System.out.println()
+
             System.out.println(getNombre() + " nada hacia la luz (fototaxis positiva).");
         } else {
-            // Usamos getNombre() y System.out.println()
+
             System.out.println(getNombre() + " busca materia en la oscuridad.");
         }
     }
     
-    // --- Getters y Setters ---
     
     public boolean isHayLuz() {
         return hayLuz;
     }
 
-    // (Este es el método setHayLuz que tu AppBioProto.java está buscando)
     public void setHayLuz(boolean hayLuz) {
         this.hayLuz = hayLuz;
     }
--- ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Flagelados.java
+++ ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Flagelados.java
@@ -1,43 +1,34 @@
 package pkBioProto.pkReinoViviente.pkProtoctista;
 
-// Clase abstracta, 'padre' de Euglena
-// Hereda de Protozoos (como Esporozoos y Ciliados)
 public abstract class Flagelados extends Protozoos {
 
-    // Atributo que comparten todos los flagelados
+
     private int cantidadFlagelos;
 
-    // --- ESTE ES EL CONSTRUCTOR QUE FALTA ---
-    // (String nombre, String habitat, int cantidadFlagelos)
-    // Coincide con la llamada super(String, String, int) de Euglena
     public Flagelados(String nombre, String habitat, int cantidadFlagelos) {
         // 'super' llama al constructor de Protozoos
         super(nombre, "Flagelado", habitat); 
         this.cantidadFlagelos = cantidadFlagelos;
     }
-    // ------------------------------------------
 
-    // --- Metodos que heredan los hijos (Euglena) ---
+    // Método que hereda a euglena
 
-    // Corregido a 'void' para ser compatible con SerVivo
+   
     @Override
     public void desplazarse() {
-        // Usamos getNombre() porque 'nombre' es private en SerVivo
+
         System.out.println(getNombre() + " se desplaza agitando su(s) " + cantidadFlagelos + " flagelo(s).");
     }
 
-    // Corregido a 'void' para ser compatible con SerVivo
     @Override
     public void reproducirse() {
-        // Usamos getNombre()
+
         System.out.println(getNombre() + " se reproduce por fisión binaria longitudinal.");
     }
 
-    // Getter
+   
     public int getCantidadFlagelos() {
         return cantidadFlagelos;
     }
-    
-    // Dejamos que Euglena implemente 'alimentarse()' 
-    // y 'reaccionarEstimulo()'
+
 }
