2669a540c179634ae50d1e2f5598aa15c239ad08
451750 <mathias.mejia@epn.edu.ec>
2025-11-11 22:43:58 -0500

    cambio en diagrama de clase, euglenas y flagelados

--- ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Flagelados.java
+++ ReinoProtista/src/pkBioProto/pkReinoViviente/pkProtoctista/Flagelados.java
@@ -1,4 +1,31 @@
 package pkBioProto.pkReinoViviente.pkProtoctista;
-public abstract class Flagelados extends Protozoos{
+
+public abstract class Flagelados extends Protozoos {
+
+    private int cantidadFlagelos;
+
+    // Constructor
+    public Flagelados(String nombre, String habitat, int cantidadFlagelos) {
+        
+        super(nombre, "Flagelado", habitat); 
+        this.cantidadFlagelos = cantidadFlagelos;
+    }
+
+    // Método que se hereda 
+
+    @Override
+    public void desplazarse() {
+        System.out.println(getNombre() + " se desplaza agitando su(s) " + cantidadFlagelos + " flagelo(s).");
+    }
+
+    @Override
+    public void reproducirse() {
+        // Usamos getNombre()
+        System.out.println(getNombre() + " se reproduce por fisión binaria longitudinal.");
+    }
+
+    public int getCantidadFlagelos() {
+        return cantidadFlagelos;
+    }
     
 }
